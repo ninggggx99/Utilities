2687d75f95253c2ea0c2d296e382c36d81627931
ninggggx99 <qingning99@gmail.com>
2020-10-02 17:38:15 +0800

    Fixed checkstyle issues

--- src/main/java/org/utils/MyUtils.java
+++ src/main/java/org/utils/MyUtils.java
@@ -12,12 +12,16 @@ public class MyUtils {
         StringParser sp = new StringParser();
 
         String str = "Hello|World|How are|you all";
-
-        List<String> stringParts = sp.splitString(str, "\\|");
-
-        for (String part : stringParts) {
-            System.out.println(part);
+        try {
+            List<String> stringParts = sp.splitString(str, "\\|");
+            for (String part : stringParts) {
+                System.out.println(part);
+            }
+        } catch (InvalidStringInputException e) {
+            e.printStackTrace();
         }
+
+
     }
 
 }
